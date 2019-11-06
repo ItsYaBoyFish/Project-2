7d2ff1c6c9ac99ca13df3b7cdd93b95c87dc1b63
Max-M <maxnmagura@gmail.com>
2019-11-05 20:14:24 -0600

    wrote schema.sql

--- models/schema.sql
+++ models/schema.sql
@@ -1,5 +1,30 @@
-DROP DATABASE IF EXISTS exampledb;
-CREATE DATABASE exampledb;
+DROP DATABASE IF EXISTS giftListApp;
+CREATE DATABASE giftListApp;
 
-DROP DATABASE IF EXISTS testdb;
-CREATE DATABASE testdb;
+USE giftListApp;
+
+CREATE TABLE users (
+  userID int NOT NULL AUTO_INCREMENT,
+  email varchar(200) NOT NULL,
+  handle varchar(200) NOT NULL,
+  username varchar(200) NOT NULL,
+  user_password varchar(200) NOT NULL,
+  PRIMARY KEY (userID)
+);
+
+CREATE TABLE recipient (
+  recipientID int NOT NULL AUTO_INCREMENT,
+  recipient_name varchar(200) NOT NULL,
+  userID int NOT NULL,
+  PRIMARY KEY (recipientID)
+);
+
+CREATE TABLE gifts (
+  giftID int NOT NULL AUTO_INCREMENT,
+  gift_name varchar(200) NOT NULL,
+  purchased boolean NOT NULL,
+  gift_url varchar(255) NOT NULL,
+  receipt varchar(200) NOT NULL,
+  recipientID int NOT NULL,
+  PRIMARY KEY (giftID)
+);
