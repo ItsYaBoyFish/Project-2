625ca7c2af1d1920e321e350236cf4da2e516d3f
Max-M <maxnmagura@gmail.com>
2019-11-07 19:42:02 -0600

    added gift delete route

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,26 +1,26 @@
 var db = require("../models");
 
 module.exports = function(app) {
-  // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
-    });
-  });
+  // // Get all examples
+  // app.get("/api/examples", function(req, res) {
+  //   db.Example.findAll({}).then(function(dbExamples) {
+  //     res.json(dbExamples);
+  //   });
+  // });
 
-  // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
-    });
-  });
+  // // Create a new example
+  // app.post("/api/examples", function(req, res) {
+  //   db.Example.create(req.body).then(function(dbExample) {
+  //     res.json(dbExample);
+  //   });
+  // });
 
-  // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.json(dbExample);
-    });
-  });
+  // // Delete an example by id
+  // app.delete("/api/examples/:id", function(req, res) {
+  //   db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
+  //     res.json(dbExample);
+  //   });
+  // });
 
   app.get("/api/gifts", function(req, res) {
     console.log("hey butt face");
@@ -33,6 +33,16 @@ module.exports = function(app) {
     db.Gift.findAll({
       where: {
         recipientID: req.params.id
+      }
+    }).then(function(dbGift) {
+      res.json(dbGift);
+    });
+  });
+
+  app.delete("/api/gifts/:id", function(req, res) {
+    db.Gift.destroy({
+      where: {
+        giftID: req.params.id
       }
     }).then(function(dbGift) {
       res.json(dbGift);
