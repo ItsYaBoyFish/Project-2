87df8f2e4d0b8b6abcf53f9b03fa01b746e34182
Austin Graves <austingraves106@aol.com>
2019-11-12 19:14:21 -0600

    I hooked up the UI to the backend. Finished alerts for logging when username or password does not match. Got all the gift routes to work appropriately and tied into the login and create forms. The last step is recipients routes and finishing touches on ensuring everything works.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -25,7 +25,7 @@ module.exports = function(app) {
   app.get("/api/gifts", function(req, res) {
     console.log("hey butt face");
     db.Gift.findAll({}).then(function(dbGift) {
-      res.json(dbGift);
+      res.send(dbGift);
     });
   });
 
@@ -34,8 +34,26 @@ module.exports = function(app) {
       where: {
         recipientID: req.params.id
       }
-    }).then(function(dbGift) {
-      res.json(dbGift);
+    }).then(function(result) {
+      // Setting up the holder arrays
+      var purchased = [];
+      var notPurchased = [];
+      // Take the results given and loop through them to determine if they are purchased or not.
+      for (var i = 0; i < result.length; i++) {
+        if (result[i].purchased === true) {
+          purchased.push(result[i]);
+        } else {
+          notPurchased.push(result[i]);
+        };
+      } 
+      // Packaging the info for delivery to UI
+      var data = {
+        layout: 'gift',
+        giftsPurchased: purchased,
+        giftsNotPurchased: notPurchased
+      }
+      // Sending the info to the client
+      res.render('gifts', data);
     });
   });
 
@@ -50,15 +68,42 @@ module.exports = function(app) {
   });
 
   app.put("/api/gifts/:id", function(req, res) {
-    db.Gift.update(
-      { purchased: 1},
-      { where: { giftID: req.params.id } }
-    ).then(function(dbGift) {
-      res.json(dbGift);
+    console.log('Update route');
+    db.Gift.findAll({
+      where: {giftID: req.params.id}
+    }).then(function(response) {
+      var purchased = response[0].dataValues.purchased;
+
+      if (purchased === false) {
+        // If the purchased value is false, set it true. 
+        db.Gift.update(
+          { purchased: 1},
+          { where: { giftID: req.params.id } }
+        ).then(function(dbGift) {
+          res.send(dbGift);
+        });
+      } else {
+        db.Gift.update(
+          { purchased: 0},
+          { where: { giftID: req.params.id } }
+        ).then(function(dbGift) {
+          res.send(dbGift);
+        });
+      }
     });
+    
+    
+    
+    // db.Gift.update(
+    //   { purchased: 1},
+    //   { where: { giftID: req.params.id } }
+    // ).then(function(dbGift) {
+    //   res.json(dbGift);
+    // });
   });
 
   app.post("/api/gifts", function(req, res) {
+    console.log(req.body);
     db.Gift.create(req.body).then(function(dbGift) {
       res.json(dbGift);
     });
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -12,6 +12,55 @@ module.exports = function(app) {
     res.render('home');
   });
 
+  app.get('/dashboard', (req, res) => {
+    var data = {
+      layout: 'dashboard',
+    people: [
+        {
+          recipient_name: 'Austin',
+          recipient_id: '1',
+          user_id: '1'
+        },
+        {
+          recipient_name: 'Joel',
+          recipient_id: '2',
+          user_id: '1'
+        },
+        {
+          recipient_name: 'Scott',
+          recipient_id: '3',
+          user_id: '1'
+        },
+        {
+          recipient_name: 'Max',
+          recipient_id: '4',
+          user_id: '1'
+        },
+        {
+          recipient_name: 'test 1',
+          recipient_id: '5',
+          user_id: '1'
+        },
+        {
+          recipient_name: 'test 2',
+          recipient_id: '6',
+          user_id: '1'
+        },
+        {
+          recipient_name: 'test 3',
+          recipient_id: '7',
+          user_id: '1'
+        },
+        {
+          recipient_name: 'test 4',
+          recipient_id: '8',
+          user_id: '1'
+        }
+      ]
+    }
+    res.render('dashboard', data);
+  });
+
   // Load example page and pass in an example by id
   app.get("/example/:id", function(req, res) {
     db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
@@ -27,10 +76,14 @@ module.exports = function(app) {
     // var userName = "testUser1";
     // var password = "ABC123";
 
-    console.log(req.body);
+    // console.log(req.body);
     
+    // Flag Set
+    var userFound = false;
+    var userInfo;
+
     db.users.findAll({}).then(function(dbusers) {
-      res.json(dbusers);
+      // res.json(dbusers);
       dbusers.forEach(function(element){
         if (req.body.username == element.dataValues.username){
           // console.log(element.dataValues.userID);
@@ -38,6 +91,8 @@ module.exports = function(app) {
           if(req.body.password === element.dataValues.user_password) {
             //Render Home Page with UserID (AUSTIN)
             console.log("Username and Password Correct for ID: ", element.dataValues.userID);
+            userFound = true;
+            userInfo = element;
           } else {
             //Alert incorrect password or redirect
             console.log("Incorrect Password");
@@ -48,7 +103,23 @@ module.exports = function(app) {
           console.log("Incorrect Username")
           // Username not in database
         }
+        // console.log(`Iteration Value: ${userFound}`);
       });
+      console.log(`Ending Value: ${userFound}`);
+      if (userFound === true) {
+        var ifData = {
+          successful: true,
+          message: 'Successfully Logged In',
+          userInfo: userInfo
+        }
+        res.send(ifData);
+      } else {
+        var elseData = {
+          successful: false,
+          message: 'Username and Password are incorrect'
+        }
+        res.send(elseData);
+      }
     });
   });
 
@@ -72,11 +143,11 @@ module.exports = function(app) {
   //     });
   // });  
 
-  app.post("/createUser", function(req, res){
+  app.post("/createUser", function(req, res) {
     var flag = 0;
     console.log(req.body);
     db.users.findAll({}).then(function(dbusers) {
-      res.json(dbusers);
+      // res.json(dbusers);
       dbusers.forEach(function(element) {
         if (req.body.username === element.dataValues.username){
           //Alert User Already Exist
@@ -98,6 +169,13 @@ module.exports = function(app) {
           handle: `@${req.body.username}`,
           username: req.body.username,
           user_password: req.body.password
+        })
+        // testing to see if i can get the same username information back.
+        .then(function(response) {
+          db.users.findAll({ where: {username: req.body.username}}).then(function(response) {
+            console.log(response);
+            res.send(response[0].dataValues);
+          })
         });
       }
     });
